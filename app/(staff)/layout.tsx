befca0211ec080ad737234ea05471ebb5ca2f968
MisielAli <mamr.213@gmail.com>
2026-08-17 15:37:12 -0700

    paso 3 y 4 completados

--- app/(staff)/layout.tsx
+++ app/(staff)/layout.tsx
@@ -1,10 +1,13 @@
 import { Sidebar } from "@/components/sidebar/Sidebar";
+import { StaffProvider } from "@/components/staff/StaffProvider";
 
 export default function StaffLayout({ children }: LayoutProps<"/">) {
   return (
-    <div className="flex min-h-screen bg-background">
-      <Sidebar />
-      <main className="h-screen min-w-0 flex-1 overflow-y-auto">{children}</main>
-    </div>
+    <StaffProvider>
+      <div className="flex min-h-screen bg-background">
+        <Sidebar />
+        <main className="h-screen min-w-0 flex-1 overflow-y-auto">{children}</main>
+      </div>
+    </StaffProvider>
   );
 }
